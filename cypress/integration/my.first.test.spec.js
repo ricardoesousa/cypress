fab7bafa32748c7b3e66bd7d5584100a10d8e4e6
Evangelista De Sousa <roes@gft.com>
2020-06-10 10:38:47 -0300

    1ª aula - Encontro de QAs - Cypress

---
+++ cypress/integration/my.first.test.spec.js
@@ -0,0 +1,10 @@
+﻿describe('My First Test', function () {
+    it('Visits the kitchen sink', function() {
+        //Arrange
+        cy.visit('https://agapito-server.herokuapp.com')
+        //Act
+        cy.get('#users').click()
+        //Assert
+        cy.get('#titulo').should('have.text','Usuários')
+    })
+})
